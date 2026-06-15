60e578535d2f9ea2dc99bc1c264be41829de552c
Antik <arthur.steyer@outlook.de>
2026-06-15 15:55:02 +0200

    Initial commit

---
+++ src/main/java/net/antik16384/paintingsneedglass/PaintingsNeedGlass.java
@@ -0,0 +1,16 @@
+package net.antik16384.paintingsneedglass;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PaintingsNeedGlass implements ModInitializer {
+	public static final String MOD_ID = "paintingsneedglass";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/antik16384/paintingsneedglass/PaintingsNeedGlassClient.java
@@ -0,0 +1,10 @@
+package net.antik16384.paintingsneedglass;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PaintingsNeedGlassClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
